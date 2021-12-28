c1b96c6005443c202cb42a637d491f6224c663ee
Wei Tang <wei@that.world>
2021-12-28 12:17:12 +0000

    Remove old nix builder, completely migrate to rust builder

--- essay.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-let
-  documents = import ./documents.nix { inherit pkgs; };
-
-in documents {
-  name = "essay";
-  source = ./sites/essay;
-}
--- specs.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-let
-  corepaper = import ./corepaper.nix { inherit pkgs; };
-  corepaper-specs = "${corepaper.with-data}/_specs.json";
-
-  template = import ./layout { inherit pkgs; };
-
-in template.spec { name = "specs"; sources = corepaper-specs; }
